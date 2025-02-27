e92c2c2c6a985b5e274a235ceefeaa71d98966ae
Earl006 <earljoe06@gmail.com>
2025-02-27 17:00:02 +0300

    [Added]: footer section

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./global/navbar/navbar.component";
 import { LandingComponent } from "./global/landing/landing.component";
+import { FooterComponent } from "./global/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, LandingComponent],
+  imports: [RouterOutlet, NavbarComponent, LandingComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
